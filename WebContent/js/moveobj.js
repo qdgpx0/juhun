aa64f8ab570d6b4ac4f0a4797daed7e8acdbf84b
qdgpx0 <116099656@qq.com>
2015-06-09 22:21:28 +0800

    Revert "1.0"
    This reverts commit 8286cd6037ce3e3f44c3a8c0e8e198526351cb6c.

--- WebContent/js/moveobj.js
+++
@@ -1,3 +0,0 @@
-$.extend({MoveObj:function(THandle,TObj,e){e=e?e:window.event;var DoMove=false;var M_offsetX=0;var M_offsetY=0;TObj.css('position','absolute');with(THandle){css('cursor','move');mousedown(function(e){DoMove=true;M_offsetX=e.offsetX?e.offsetX:e.layerX;M_offsetY=e.offsetY?e.offsetY:e.layerY;});mouseup(function(){DoMove=false;});}
-$(document).mousemove(function(e){if(!DoMove)
-return;var obj_x=e.clientX- M_offsetX;var obj_y=e.clientY- M_offsetY;with(TObj){css("left",obj_x);css("top",obj_y);}});}});
--- WebContent/js/setobjcenter.js
+++
@@ -1,2 +0,0 @@
-function SetObjCenter(Tobj,tag){switch(tag){case 1:with(Tobj){css('position','absolute');css('left','50%');css('top','50%');css('margin-top',(0-parseInt(height()))/2+'px');css('margin-left',(0-parseInt(width()))/2+'px');}
-case 2:with(Tobj){css('position','absolute');css('left',($(document).width()-width())/2 + 'px');css('top',($(document).height()-height())/2 + 'px');}}}
